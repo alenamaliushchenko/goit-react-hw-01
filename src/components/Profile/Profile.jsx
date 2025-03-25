b2d1fb777d305d935073ed9ae90d4dc53cbb4a10
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-03-25 16:07:00 +0200

    added task 1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+import css from './Profile.module.css'
+
+export default function Profile ({name, tag, location, image, stats}) {
+    return (
+        <div className={css.profileContainer}>
+          <div className={css.profile}>
+            <img
+              src={image}
+              alt="User avatar"
+        className={css.avatar}
+            />
+            <p className={css.name}>{name}</p>
+            <p className={css.tag}>@{tag}</p>
+            <p className={css.location}>{location}</p>
+          </div>
+
+          <ul className={css.statsList}>
+            <li className={css.statsItem}>
+              <span className={css.statsLabel}>Followers</span>
+              <span className={css.statsValue}>{stats.followers}</span>
+            </li>
+            <li className={css.statsItem}>
+              <span className={css.statsLabel}>Views</span>
+              <span className={css.statsValue}>{stats.views}</span>
+            </li>
+            <li className={css.statsItem}>
+              <span className={css.statsLabel}>Likes</span>
+              <span className={css.statsValue}>{stats.likes}</span>
+            </li>
+          </ul>
+        </div>
+    );    
+};
