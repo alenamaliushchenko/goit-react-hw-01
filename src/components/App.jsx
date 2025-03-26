e21282ce9f5ebd33b49d884e5d218c13c4ffa5ff
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-03-26 13:30:18 +0200

    added task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Profile from './Profile/Profile'
 import userData from '../userData.json'
 import FriendList from './FriendList/FriendList'
 import friends from '../friends.json'
+import transactions from '../transactions.json'
+import TransactionHistory from './TransactionHistory/TransactionHistory'
 
 
 const App = () => {
@@ -16,7 +18,8 @@ const App = () => {
           stats={userData.stats}
         />
         <FriendList friends={friends} />
-      </>
+        <TransactionHistory items={transactions} />
+      </>
     );
-  };
+};
 export default App
